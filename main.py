4dcd17ee929c954b068a04caa9a7f3805a02ac28
hid3rx <hid3rx@hotmail.com>
2025-12-12 14:37:56 +0800

    小改动

--- main.py
+++ main.py
@@ -57,6 +57,7 @@ configs = \
     "headers": {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/114.0",
         "Connection": "close",
+        # "X-Requested-With": "XMLHttpRequest"
     },
 
     # 自定义cookies
